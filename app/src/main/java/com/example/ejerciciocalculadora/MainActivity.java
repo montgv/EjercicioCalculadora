7e3428ba5ee0bcf8da99a7dc4afd1d867ad7685d
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-22 11:28:29 +0200

    Commit terminada y comentado

--- app/src/main/java/com/example/ejerciciocalculadora/MainActivity.java
+++ app/src/main/java/com/example/ejerciciocalculadora/MainActivity.java
@@ -11,18 +11,26 @@ import com.example.ejerciciocalculadora.databinding.ActivityMainBinding;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Creamos el binding que nos sirve para la vinculación de la vista
     private ActivityMainBinding binding;
 
+    //Creamos la diferentes variables que las vamos a usar para las operaciones
     private int numero1, numero2, operacion = 0;
 
+    //Este es el metodo que se llama para que comienze el activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Estas tres lineas hace que se nos vincule las vistas con el activity
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton0.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -35,7 +43,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton1.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -48,7 +61,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton2.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -61,7 +79,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton3.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -74,7 +97,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton4.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -87,7 +115,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton5.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -100,7 +133,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton6.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -113,7 +151,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton7.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -126,7 +169,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton8.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -139,7 +187,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado cambiamos para que ponga el texto de ese boton, sino en idResultado
+        // cambiamos para que se añada a lo que tiene, el texto de ese boton.
         binding.idBoton9.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -152,12 +205,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado muestre su contenido, sino en idResultado cambiamos para que se añada a lo
+        //que tiene, el texto de ese boton.
         binding.idBotonMas.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
                 if (binding.idResultado.getText().toString().equals("0")) {
-                    binding.idResultado.setText(binding.idBotonMas.getText());
+                    binding.idResultado.getText();
                 } else {
                     binding.idResultado.setText((binding.idResultado.getText().toString()) +
                             (binding.idBotonMas.getText().toString()));
@@ -165,7 +223,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado muestre su contenido, sino en idResultado cambiamos para que se añada a lo
+        //que tiene, el texto de ese boton.
         binding.idBotonMenos.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -178,7 +241,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado muestre su contenido, sino en idResultado cambiamos para que se añada a lo
+        //que tiene, el texto de ese boton.
         binding.idBotonPor.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -191,7 +259,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón si idResultado contiene un 0 entonces
+        //en idResultado muestre su contenido, sino en idResultado cambiamos para que se añada a lo
+        //que tiene, el texto de ese boton.
         binding.idBotonDivide.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
@@ -204,16 +277,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón, comento linea a linea.
         binding.idBotonIgual.setOnClickListener(new View.OnClickListener() {
+            //Indica que se debe ignorar las advertencias especificas
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
+                //Declaramos una variable de tipo string que contiene lo que tiene idResultado en
+                //ese momento.
                 String pantalla = binding.idResultado.getText().toString();
-                String[]operadores = new String[]{"\\+", "\\-", "\\/", "\\*"};
+                //Declaramos un array de string para que guarde los diferentes operadores
+                String[] operadores = new String[]{"\\+", "\\-", "\\/", "\\*"};
+                //Declaramos un array de string para que con ayuda del metodo split separemos los
+                //numeros del operador de la variable pantalla
                 String[] numSeparados = pantalla.split(Arrays.toString(operadores));
                 numero1 = Integer.parseInt(numSeparados[0]);
                 numero2 = Integer.parseInt(numSeparados[1]);
 
+                //Si idResultado asociado al binding devuelve su texto y contiene el operador indicado
+                //entonces dependiendo de cual sea el operador se realiza la operacion y despues
+                //cambiamos idResultado con setText y como operacion es un numero, lo convertimos a
+                //string
                 if (binding.idResultado.getText().toString().contains("+")) {
                     operacion = numero1 + numero2;
                     binding.idResultado.setText(String.valueOf(operacion));
@@ -230,6 +315,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al
+        //boton que se ve y al binding, cuando se pulse el botón, las variable que se ven se inicializan
+        //a 0, y idResultado asociado al binding con el metodo setText cambiamos el texto a 0,
+        // es una manera de borrar los datos.
         binding.idResultado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -240,5 +329,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
